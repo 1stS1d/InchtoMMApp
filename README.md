ed3d93882861d089d7ed26cf52cf33cddd1fb83a
1stS1d <jack7nugget@gmail.com>
2020-05-29 16:28:02 -0700

    v0.01

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ For converting between inches and millimeters quickly.
 Based on the INMMCONV.PY program from https://github.com/1stS1d/mklotz
 
 
+## Change-log:
+
+v0.0.1:
+- Started basic formation of the layout
+
+
+
+
+-----
 This app is made using:
 - `Briefcase`: https://github.com/beeware/briefcase
 - `The BeeWare Project`: https://beeware.org/
--- inmmconv/src/inmmconv/app.py
+++ inmmconv/src/inmmconv/app.py
@@ -3,7 +3,7 @@ For converting between inches and millimeters quickly.
 """
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
+from toga.style.pack import *
 
 
 class InchtoMMApp(toga.App):
@@ -17,6 +17,37 @@ class InchtoMMApp(toga.App):
         show the main window.
         """
         main_box = toga.Box()
+
+        titleLabel = toga.Label("Quick Inch <-> MM Conversion Tool", style=Pack(text_align=LEFT))
+
+        left_container = toga.Box()
+
+        left_container.add(titleLabel)
+
+
+
+        data = [("TEST", "TEST 2", "TEST 3"), ("TEST 4", "TEST 5", "TEST 6")]
+
+        right_container = toga.Table(headings=['INCH DEC', "INCH FRAC", "MILLIMETERS"], data=data)
+
+        right_container.style.update(width=400)
+
+
+        left_container.style.update(width=400, padding_left=10)
+
+        split = toga.SplitContainer(direction="VERTICAL")
+
+        split.style.update(width=1000)
+
+        split.content = [right_container, left_container]
+
+        main_box.add(split)
+
+        main_box.style.update(direction=COLUMN, padding_top=10)
+
+        titleLabel.style.update(width=350, padding_left=10, padding_bottom=10)
+
+
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
